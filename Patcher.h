f1fcd58a300d8eb5e473128367de3985671c95f3
BradDorney <25650206+BradDorney@users.noreply.github.com>
2025-04-27 01:59:20 -0400

    Update copyright

--- Patcher.h
+++ Patcher.h
@@ -1,6 +1,6 @@
 /*
  ***********************************************************************************************************************
- * Copyright (c) 2022, Brad Dorney
+ * Copyright (c) 2025, Brad Dorney
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
